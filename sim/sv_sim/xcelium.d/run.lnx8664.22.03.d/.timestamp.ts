0e399aace09ce7c8dafc4282914d3f5c8657e3a9
Cristian Cristea <cristian.cristea@stud.etti.upb.ro>
2023-11-24 00:38:23 +0200

    Remove obsolete files

--- sim/sv_sim/xcelium.d/run.lnx8664.22.03.d/.timestamp.ts
+++
@@ -1,9 +0,0 @@
-1653901138 /home/matache.dragos/aces_fv_proj/source/sv/transmitter_tb/tb/uart_transmitter_tb.sv
-1698158323 /home/matache.dragos/aces_fv_proj/source/v/rtl/UART_TRANSMITTER.v
-1653901593 /home/matache.dragos/aces_fv_proj/source/sv/transmitter_tb/include/env/env_pkg.sv
-1698157410 /home/matache.dragos/aces_fv_proj/source/v/rtl/TX_BAUD_GEN.v
-1698157826 /home/matache.dragos/aces_fv_proj/source/v/rtl/FIFO_BUFFER.v
-1653901271 /home/matache.dragos/aces_fv_proj/source/sv/transmitter_tb/include/tests/test_pkg.sv
-1686914340 /home/matache.dragos/aces_fv_proj/source/v/rtl/UART_TRANSMITTER_TB.v
-1653901075 /home/matache.dragos/aces_fv_proj/source/sv/transmitter_tb/tb/sys_if.sv
-1698158442 /home/matache.dragos/aces_fv_proj/source/v/rtl/UART_TX.v
--- sim/sv_sim/xcelium.d/run.lnx8664.22.03.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1647972022 /opt/cadence/installs/XCELIUM2203/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
-1647972019 /opt/cadence/installs/XCELIUM2203/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
