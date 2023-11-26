d5ce27e01c75c48e4720d4d7af2fec5f2145c4bf
Cristian Cristea <cristian.cristea@stud.etti.upb.ro>
2023-11-26 12:59:50 +0200

    Bug #3

--- source/v/rtl/TX_BAUD_GEN.v
+++ source/v/rtl/TX_BAUD_GEN.v
@@ -16,10 +16,10 @@ end
 
 always@(*) begin
     case(baudrate_select)
-        2'b01: tx_baud = counter[3:0] == 4'b1111;
-        2'b00: tx_baud = counter[4:0] == 5'b11111;
-        2'b11: tx_baud = counter[5:0] == 6'b111111;
-        2'b10: tx_baud = counter[6:0] == 7'b1111111;
+        2'b00: tx_baud = counter[3:0] == 4'b1111;
+        2'b01: tx_baud = counter[4:0] == 5'b11111;
+        2'b10: tx_baud = counter[5:0] == 6'b111111;
+        2'b11: tx_baud = counter[6:0] == 7'b1111111;
         default: tx_baud = counter[0]; 
     endcase
 end    
