41a6e0323ead2cbf284bfb2bdc41b105863e3176
Cristian Cristea <cristian.cristea@stud.etti.upb.ro>
2023-12-15 10:13:12 +0200

    Bug #8

--- source/rtl/FIFO_BUFFER.v
+++ source/rtl/FIFO_BUFFER.v
@@ -36,9 +36,9 @@ always@(posedge clock) begin
     if(reset_n == 0)
         fifo_counter <= 0;
     else if (rd_en_mem && !wr_en_mem)
-        fifo_counter <= fifo_counter + 1;
+        fifo_counter <= fifo_counter - 1;
     else if (!rd_en_mem && wr_en_mem)
-        fifo_counter <= fifo_counter - 1;	
+        fifo_counter <= fifo_counter + 1;	
     end
  
 //READ_ENABLE AND WRITE_ENABLE
